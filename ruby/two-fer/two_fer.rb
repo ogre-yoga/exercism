ca4bc4820f0a5ff5eb038829b98aa9a6bf056a49
ogre-yoga <me@ogre.yoga>
2020-01-09 19:58:33 +1100

    Complete 02 two-fer with string interpolation

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,11 +1,5 @@
 class TwoFer
-	def self.two_fer( name="none" ) #default value for arg
-		if name == "Alice"
-			"One for Alice, one for me."
-		elsif name == "Bob"
-			"One for Bob, one for me."
-		elsif name ==  "none"
-			"One for you, one for me."
-		end	
+	def self.two_fer(name="you") #default value for arg
+		"One for #{name}, one for me." #string interpolation
 	end
 end
