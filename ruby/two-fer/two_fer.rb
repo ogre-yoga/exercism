77eada31322d24c254e3cb07cd589ec95c25764b
Adam Alexander <adam@bwdstrategic.com>
2020-01-10 10:31:36 +1100

    Revise ruby 02 two-fer as per mentor comments

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,5 +1,13 @@
 class TwoFer
-	def self.two_fer(name="you") #default value for arg
-		"One for #{name}, one for me." #string interpolation
-	end
+  def self.two_fer(recipient='you') # Descriptive, unabbreviated argument name with default value
+    "One for #{recipient}, one for me." # string interpolation: passed
+    # 'One for ' + recipient + ', one for me.' # string concatenation: passed
+    # sprintf("One for %{who}, one for me.", {:who => recipient}) # functional-style sprintf method: passed
+    # "One for %s, one for me." % recipient # sprintf string template: passed
+  end
 end
+
+# REFERENCES
+# https://ruby-doc.org/core-2.7.0/Kernel.html#method-i-sprintf
+# https://ruby-doc.org/core-2.4.0/String.html#method-i-25
+# More on format strings: https://idiosyncratic-ruby.com/49-what-the-format.html
