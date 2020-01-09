457a67e0828cb3177f890e2e90a319e12bb07f9c
Adam Alexander <adam@bwdstrategic.com>
2020-01-09 17:36:42 +1100

    Complete 2. two-fer [TODO] simplify return string

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,8 +1,11 @@
-=begin
-Write your code for the 'Two Fer' exercise in this file. Make the tests in
-`two_fer_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/two-fer` directory.
-=end
-
+class TwoFer
+	def self.two_fer( name="none" ) #default value for arg
+		if name == "Alice"
+			"One for Alice, one for me."
+		elsif name == "Bob"
+			"One for Bob, one for me."
+		elsif name ==  "none"
+			"One for you, one for me."
+		end	
+	end
+end
