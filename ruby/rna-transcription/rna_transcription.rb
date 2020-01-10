098f26b0f3e739a1c8d70527f00a0d5ef2073e1f
Adam Alexander <adam@bwdstrategic.com>
2020-01-10 16:59:04 +1100

    Add references to ruby 02B solution

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -14,3 +14,41 @@ class Complement
 
   end
 end
+
+
+=begin
+  
+COMMUNITY SOLUTIONS
+
+https://exercism.io/tracks/ruby/exercises/rna-transcription/solutions/9c29e0dafef7cba71a4b466f
+
+class Complement
+
+  DNA = 'CATG'
+  RNA = 'GUAC'#amole
+
+  def self.of_dna(nucleotides)
+    nucleotides.tr(DNA, RNA)
+  end
+
+  def self.of_rna(nucleotides)
+    nucleotides.tr(RNA, DNA)
+  end
+
+end
+
+https://exercism.io/tracks/ruby/exercises/rna-transcription/solutions/099bbf6ffb9b7dae637ba16f
+
+class Complement
+  
+  def self.of_dna(strand)
+    strand.tr('GCTA', 'CGAU')
+  end
+
+  def self.of_rna(strand)
+    strand.tr('CGAU', 'GCTA')
+  end
+
+end
+  
+=end
