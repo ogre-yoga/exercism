ca4b1cbdaaf2c98646efb81c6ff4a8755891b00d
Adam Alexander <adam@bwdstrategic.com>
2020-01-10 12:46:49 +1100

    Clean up comments

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,28 +1,32 @@
 class TwoFer
   def self.two_fer(recipient='you') # Descriptive, unabbreviated argument name with default value
     "One for #{recipient}, one for me." # string interpolation: passed
-    # 'One for ' + recipient + ', one for me.' # string concatenation: passed
-    # sprintf("One for %{who}, one for me.", {:who => recipient}) # functional-style sprintf method: passed
-    # "One for %s, one for me." % recipient # sprintf string template: passed
   end
 end
 
-# REFERENCES
-# https://ruby-doc.org/core-2.7.0/Kernel.html#method-i-sprintf
-# https://ruby-doc.org/core-2.4.0/String.html#method-i-25
-# More on format strings: https://idiosyncratic-ruby.com/49-what-the-format.html
-
 =begin
 
-MENTOR NOTES
+ALTERNATIVE SOLUTIONS
 
+'One for ' + recipient + ', one for me.' # string concatenation: passed
+
+sprintf("One for %{who}, one for me.", {:who => recipient}) # functional-style sprintf method: passed
+
+"One for %s, one for me." % recipient # sprintf string template: passed
+
+REFERENCES
+https://ruby-doc.org/core-2.7.0/Kernel.html#method-i-sprintf
+https://ruby-doc.org/core-2.4.0/String.html#method-i-25
+More on format strings: https://idiosyncratic-ruby.com/49-what-the-format.html
+
+MENTOR NOTES
 The notes I made in addition are here:
 
 In the "What the Format" article, the advantages of classical interpolation section lists these points:
 
-    Interpolation happens on syntax level, no method calling involved
-    Inline interpolation quicker to write/more convenient for simple interpolations
-    Not possible to mismatch values and format string references
+1. Interpolation happens on syntax level, no method calling involved
+2. Inline interpolation quicker to write/more convenient for simple interpolations
+3. Not possible to mismatch values and format string references
 
 Here are some clarifying points:
 
