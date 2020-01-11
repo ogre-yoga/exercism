b305611219d808a366c1089db3e33649f64224d8
ogre-yoga <me@ogre.yoga>
2020-01-11 18:15:33 +1100

    Complete revision of ruby 03 resistor-color-duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -3,14 +3,7 @@ class ResistorColorDuo
   COLORS = %w[black brown red orange yellow green blue violet grey white]
 
   def self.value(band_colors)
-    band_colors = band_colors.slice(0,2)
-    band_value = Array.new
-
-    band_colors.each do |color|
-      band_value.push(COLORS.index(color))
-    end
-
-    band_value.join.to_i
+    band_colors.map{|color| COLORS.index(color)}.slice(0,2).join.to_i
   end
 
 end
