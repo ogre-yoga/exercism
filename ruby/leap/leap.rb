1ad4b0b6fe1455e7ff4a7fc15fae8a4fe0752dfc
ogre-yoga <me@ogre.yoga>
2020-01-12 16:32:43 +1100

    added footnotes to ruby leap solution - plenty to learn here in terms of elegance

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -10,3 +10,54 @@ class Year
   end
 end
 
+
+
+=begin
+  
+Community solutions
+
+https://exercism.io/tracks/ruby/exercises/leap/solutions/4e407b11bd6a4a14bd824845d20231b1
+
+class Year
+  def self.leap?(number)
+    number % 400 == 0 || number % 4 == 0 && number % 100 != 0
+  end
+end
+  
+
+
+https://exercism.io/tracks/ruby/exercises/leap/solutions/02e81557511cecacd7bd5321
+
+class Year
+  def self.leap?(year)
+    year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+  end
+end
+
+https://exercism.io/tracks/ruby/exercises/leap/solutions/33aa935c13f5e7872a45446f
+
+class Year
+
+  def initialize(year)
+    @year = year
+  end
+
+  def leap?
+    fourth_year? unless century? and not fourth_century?
+  end
+
+  def fourth_year?
+    @year % 4 == 0
+  end
+
+  def century?
+    @year % 100 == 0
+  end
+
+  def fourth_century?
+    @year % 400 == 0
+  end
+
+end
+
+=end
