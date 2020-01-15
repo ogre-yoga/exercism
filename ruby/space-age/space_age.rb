0fc9350ae6954fb005cfa5a80c87c83576499a2e
Adam Alexander <adam@bwdstrategic.com>
2020-01-15 17:39:42 +1100

    Added references to 04b space-age

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -52,3 +52,39 @@ class SpaceAge
   end
 
 end
+
+=begin
+  
+Community solutions
+
+https://exercism.io/tracks/ruby/exercises/space-age/solutions/a8ae3bd7154340c59c64a1d57379bdca
+
+#define_method "metaprogramming"
+
+class SpaceAge
+  EARTH_YEAR_IN_SECONDS = 31_557_600
+  ORBITAL_PERIODS_IN_EARTH_YEARS = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+  attr_reader :seconds
+
+  def initialize(seconds)
+    @seconds = seconds
+  end
+
+  ORBITAL_PERIODS_IN_EARTH_YEARS.each do |planet, orbital_period|
+    define_method('on_' + planet.to_s) do
+      seconds.to_f / (EARTH_YEAR_IN_SECONDS * orbital_period)
+    end
+  end
+end
+  
+end
