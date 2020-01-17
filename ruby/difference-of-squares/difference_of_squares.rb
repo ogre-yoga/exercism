6629e61a755c486d2b7451eaf607f4dce7175bec
Adam Alexander <adam@bwdstrategic.com>
2020-01-17 17:14:08 +1100

    Added references to ruby/differences of squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -11,7 +11,7 @@ class Squares
   end
 
   def sum_of_squares
-    range.map!{|index| index ** 2}.sum
+    range.map{ |index| index ** 2 }.sum
   end
 
   def difference
@@ -19,3 +19,96 @@ class Squares
   end
 
 end
+
+=begin
+
+Commnity solutions
+
+https://exercism.io/tracks/ruby/exercises/difference-of-squares/solutions/b9b70506ae8ddb1dea19b505
+  
+class Squares
+
+  def initialize(n)
+    @numbers = 1..n
+  end
+
+  def square_of_sums
+    @numbers.reduce(:+) ** 2
+  end
+
+  def sum_of_squares
+    @numbers.reduce { |sum, x| sum + (x * x) }
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
+
+end
+
+https://exercism.io/tracks/ruby/exercises/difference-of-squares/solutions/c1953961179142ea935d91f79d999bda
+
+class Squares
+  VERSION = 2
+
+  def initialize(num)
+    @one_to_num = 1.upto num
+  end
+
+  def square_of_sum
+    @one_to_num.reduce(0, :+) ** 2
+  end
+
+  def sum_of_squares
+    @one_to_num.map { |n| n ** 2 }.reduce(0, :+)
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
+
+https://exercism.io/tracks/ruby/exercises/difference-of-squares/solutions/e139ed2b282d630c630ee266
+
+class Squares
+  attr_reader :max
+
+  def initialize(max)
+    @max = max 
+  end
+
+  def square_of_sums
+    (0..max).inject { |sum, number| sum += number } ** 2
+  end
+
+  def sum_of_squares
+    (0..max).inject { |sum, number| sum + number**2 }
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
+
+end
+
+https://exercism.io/tracks/ruby/exercises/difference-of-squares/solutions/d76ef9bc788a4f75a6d6a31fefa2fef6
+
+class Squares
+  def initialize(number) 
+    @range = (1..number)
+  end
+
+  def square_of_sums 
+    @range.inject(:+)**2
+  end
+
+  def sum_of_squares 
+    @range.inject{|sum,x| sum + x**2 }
+  end
+
+  def difference 
+    square_of_sums - sum_of_squares
+  end
+end
+  
+=end
