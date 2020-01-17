d37d284f5f440c6e5492dd7b118cde4add60b72e
Adam Alexander <adam@bwdstrategic.com>
2020-01-17 11:59:18 +1100

    Fix typo ruby high scores

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -7,15 +7,15 @@ class HighScores
   end
 
   def latest
-    @scores.last
+    scores.last
   end
 
   def personal_best
-    @scores.max
+    scores.max
   end
 
   def personal_top_three
-    @scores.sort.reverse!.first(3)
+    scores.max(3)
   end
 
   def latest_is_personal_best?
