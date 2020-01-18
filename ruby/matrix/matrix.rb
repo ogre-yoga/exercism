b7f6f23815069a75588f95098e395b628af6cdb2
ogre-yoga <me@ogre.yoga>
2020-01-19 08:42:54 +1100

    revision to ruby matrix

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -3,7 +3,7 @@ class Matrix
   attr_reader :rows, :columns
 
   def initialize(string_of_numbers)
-    @rows = string_of_numbers.lines.map(&:to_s.split.map(&:to_i))
+    @rows = string_of_numbers.lines.map{ |line|line.split.map(&:to_i) }
     @columns = rows.transpose
   end
 
